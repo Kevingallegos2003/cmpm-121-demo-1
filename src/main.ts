430e13176dfd11c6d6f6160e4a67223f6e0e772f
Kevin Gallegos <kevingallegos2003@gmail.com>
2024-10-10 09:56:07 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing gayme";
+const gameName = "My amazing BUtton!";
 document.title = gameName;
+const button = document.createElement('button');
+button.textContent = '🎂';
 
+document.body.appendChild(button);
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
