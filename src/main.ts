86a53dcd6482f25aa43701574ae0036d65457955
Kevin Gallegos <kevingallegos2003@gmail.com>
2024-10-10 21:54:00 -0700

    Step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "./style.css";
 
+//---DEFINITIONS-------------------
 const app: HTMLDivElement = document.querySelector("#app")!;
 let count: number = 0;
 const gameName = "My amazing BUtton!";
@@ -7,15 +8,21 @@ document.title = gameName;
 const button = document.createElement("button");
 button.textContent = "🎂";
 const counter = document.createElement("p");
-counter.textContent ="Cakes eaten: " + count;
+counter.textContent = "Cakes eaten: " + count;
 document.body.appendChild(button);
 app.append(counter);
-button.addEventListener('click',()=>{ 
-    count++; 
-    console.log(count);
+
+//---------------------FUNCTIONS AND LISTENERS------------
+function eatcake(){
+    count++;
     counter.textContent = "Cakes eaten: " + count;
-    
+}
+button.addEventListener("click", () => {
+  console.log(count);
+  eatcake();
 });
+//-------------Code-----------------------
+setInterval(eatcake, 1000);
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
