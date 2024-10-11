18406435a7cee548854dc8a0d830dcff8f267943
Kevin Gallegos <kevingallegos2003@gmail.com>
2024-10-11 11:58:37 -0700

    Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import "./style.css";
 //---DEFINITIONS-------------------
 const app: HTMLDivElement = document.querySelector("#app")!;
 let count: number = 0;
+let start = performance.now();
 const gameName = "My amazing BUtton!";
 document.title = gameName;
 const button = document.createElement("button");
@@ -13,16 +14,24 @@ document.body.appendChild(button);
 app.append(counter);
 
 //---------------------FUNCTIONS AND LISTENERS------------
-function eatcake(){
-    count++;
-    counter.textContent = "Cakes eaten: " + count;
+function eatcake() {
+  count++;
+  counter.textContent = "Cakes eaten: " + count;
+}
+function step(timestamp: number){
+    if (timestamp - start >= 1000){
+      start = timestamp;
+      eatcake();
+    }
+    requestAnimationFrame(step);
 }
 button.addEventListener("click", () => {
   console.log(count);
   eatcake();
 });
 //-------------Code-----------------------
-setInterval(eatcake, 1000);
+//setInterval(eatcake, 1000);
+requestAnimationFrame(step);
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
