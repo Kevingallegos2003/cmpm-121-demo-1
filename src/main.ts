607df77342ede39aee9829cb939ab1e5559d85ea
Kevin Gallegos <kevingallegos2003@gmail.com>
2024-10-12 22:00:48 -0700

    Step 6 complete

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,24 @@ import "./style.css";
 //---DEFINITIONS-------------------
 const app: HTMLDivElement = document.querySelector("#app")!;
 let count: number = 0;
+let multiplier: number = 1;
+let rateCost:number = 10;
+let ratepurchases:number = 0;
 let start = performance.now();
 const gameName = "ALL CAKED UP";
 document.title = gameName;
 const button = document.createElement("button");
 const upgrade1 = document.createElement("button");
+const upgradeRate = document.createElement("button");
+upgradeRate.textContent = "Upgrade rate of cakes to 1.1/sec - " +rateCost+" Coins" ;
 upgrade1.textContent = "AUTO BAKE - 10 CAKES";
 button.textContent = "🎂";
 const counter = document.createElement("p");
+const DisplayRate = document.createElement("p");
+const ratepurText = document.createElement("p");
 counter.textContent = "Cakes baked: " + count;
+DisplayRate.textContent = "Baking "+multiplier+" cake per second";
+ratepurText.textContent = "Cake Rate Purchases: "+ ratepurchases;
 document.body.appendChild(button);
 app.appendChild(upgrade1);
 app.append(counter);
@@ -19,12 +28,14 @@ disabledButton();
 
 //---------------------FUNCTIONS AND LISTENERS------------
 function eatcake() {
-  count++;
+  //count++;
+  count+=multiplier;
   counter.textContent = "Cakes baked: " + count;
   disabledButton();
 }
-function disabledButton(){
+function disabledButton() {
   upgrade1.disabled = count < 10;
+  upgradeRate.disabled = count < rateCost;
 }
 function step(timestamp: number) {
   if (timestamp - start >= 1000) {
@@ -38,9 +49,34 @@ button.addEventListener("click", () => {
   eatcake();
 });
 upgrade1.addEventListener("click", () => {
-  if(count >= 10){
-    count-= 10;
+  if (count >= 10) {
+    count -= 10;
+    app.append(DisplayRate);
+    app.appendChild(upgradeRate);
+    app.appendChild(ratepurText);
     requestAnimationFrame(step);
+    upgrade1.hidden = true;
+  }
+});
+upgradeRate.addEventListener("click", () => {
+  if (count >= rateCost) {
+    count -= rateCost;
+    rateCost*=10;
+    multiplier+=0.1;
+    ratepurchases++;
+    ratepurText.textContent = "Cake Rate Purchases: "+ ratepurchases;
+    DisplayRate.textContent = "Baking "+multiplier+" cake per second";
+    if(multiplier >= 1.1){
+      upgradeRate.textContent = "Upgrade rate of cakes to 1.2/sec - " +rateCost+" Coins" ;
+    }
+    else{
+      upgradeRate.textContent = "Upgrade rate of cakes to 50/sec - " +rateCost+" Coins" ;
+    }
+
+    if(rateCost>1000){
+      multiplier = 50;
+      upgradeRate.hidden = true;
+    }
   }
 });
 //-------------Code-----------------------
